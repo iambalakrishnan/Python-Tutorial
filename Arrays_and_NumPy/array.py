482152faf7a8b98c923e494b25ab77217ffe82d5
iambalakrishnan <impythonprogrammer@gmail.com>
2021-09-17 16:41:16 +0530

    numpy

---
+++ Arrays_and_NumPy/array.py
@@ -0,0 +1,8 @@
+import array as arr
+
+val = arr.array('i',[1,2,3,4,5,6])#need to mention type code
+print(val)
+
+print(val.buffer_info())
+print(val.typecode)
+
---
+++ Arrays_and_NumPy/numpy_arr.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+arr=np.array([1,2,3,4,5,6])
+print(arr)
+print(arr.dtype)
+
+arr1=np.ones([3,2,2])
+print(arr1)
